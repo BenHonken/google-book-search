2145893026b7c0e3ae08a27a3673803be6d8a54d
Ben Honken <honkenb@gmail.com>
2020-02-29 16:05:02 -0600

    restoring previous version

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -9,7 +9,7 @@ function BookCard(props) {
   // Load all books and store them with setBooks
   useEffect(() => {
     loadBooks();
-  })
+  }, [])
 
   // Loads all books and sets them to books
   function loadBooks() {
@@ -50,7 +50,7 @@ function BookCard(props) {
               <div className="row">
               <div className="col-md-10" style={{textAlign: "left"}}>
                   <h4>{result.volumeInfo.title}</h4>
-                  <p>{result.volumeInfo.authors}</p>
+                  <p>{result.volumeInfo.authors[0]}</p>
                 </div>
                 <div className="col-md-2">
                   <ViewBtn href={result.volumeInfo.infoLink}></ViewBtn>
@@ -59,7 +59,7 @@ function BookCard(props) {
               </div>
               <div className="row">
                 <div className="col-md-2">
-                  {/* <img src={result.volumeInfo.imageLinks.thumbnail} alt={result.volumeInfo.title} style={{float: "left"}}/> */}
+                  <img src={result.volumeInfo.imageLinks.thumbnail} alt={result.volumeInfo.title} style={{float: "left"}}/>
                 </div>
                 <div className="col-md-10">
                   {result.volumeInfo.description}
