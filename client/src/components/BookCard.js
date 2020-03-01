8bdcc67c55248c447a100eb778fb4bfcd5ecc147
Ben Honken <honkenb@gmail.com>
2020-02-29 20:40:34 -0600

    adding a prepopulated array

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -4,7 +4,28 @@ import ViewBtn from "./ViewBtn"
 import API from "../utils/API"
 function BookCard(props) {
   // Setting our component's initial state
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([{volumeInfo:
+    {title: "Harry Potter: The Complete Collection (1-7)",
+    authors: ["J.K. Rowling"],
+    publisher: "Pottermore Publishing",
+    publishedDate: "2015-12-14",
+    description: "All seven eBooks in the multi-award winning, internationally bestselling Harry Potter series, available as one download with stunning cover art by Olly Moss. Enjoy the stories that have captured the imagination of millions worldwide.",
+    readingModes: {text: true, image: false},
+    pageCount: 4236,
+    printType: "BOOK",
+    categories: ["Juvenile Fiction"],
+    averageRating: 4,
+    ratingsCount: 11,
+    maturityRating: "NOT_MATURE",
+    allowAnonLogging: true,
+    contentVersion: "1.14.13.0.preview.2",
+    panelizationSummary: {containsEpubBubbles: false, containsImageBubbles: false},
+    imageLinks: {smallThumbnail: "http://books.google.com/books/content?id=f280CwAAQ…J&printsec=frontcover&img=1&zoom=5&source=gbs_api", thumbnail: "http://books.google.com/books/content?id=f280CwAAQ…J&printsec=frontcover&img=1&zoom=1&source=gbs_api"},
+    language: "en",
+    previewLink: "http://books.google.com/books?id=f280CwAAQBAJ&pg=PP1&dq=Harry+Potter&hl=&cd=1&source=gbs_api",
+    infoLink: "http://books.google.com/books?id=f280CwAAQBAJ&dq=Harry+Potter&hl=&source=gbs_api",
+    canonicalVolumeLink: "https://books.google.com/books/about/Harry_Potter_The_Complete_Collection_1_7.html?hl=&id=f280CwAAQBAJ",
+    __proto__: Object}}])
 
   // Load all books and store them with setBooks
   useEffect(() => {
